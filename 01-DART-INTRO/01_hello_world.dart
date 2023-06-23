16db7c287de74ce5646d1d5cd96652cefd04a7e5
Roque Molina <roque.nicolas.molina@gmail.com>
2023-06-22 23:08:16 -0300

    02-var

--- 01-DART-INTRO/01_hello_world.dart
+++ 01-DART-INTRO/01_hello_world.dart
@@ -1,8 +1,10 @@
-void main () {
-  late final String myName;
-  
-  myName = "ala";
-  
+void main() {
+//    var myName ='Roque';
+//    late final myName;
+//    String nyName='Roque';
+  const myName = "ala";
+
   print('Hola ${myName.toUpperCase()}');
-  
+  print('Hola ${myName.toLowerCase()}');
+  print('Hola ${3 + 5}');
 }
