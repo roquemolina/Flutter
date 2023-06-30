5f081cfb5ca8d6513094e3719bc21e71ab25aefc
Roque Molina <roque.nicolas.molina@gmail.com>
2023-06-30 01:03:34 -0300

    siss

--- tikitoki/lib/presentation/widgets/shared/video_buttons.dart
+++ tikitoki/lib/presentation/widgets/shared/video_buttons.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../../../domain/entities/videos_post.dart';
+
 class VideoButtons extends StatelessWidget {
-  const VideoButtons({super.key});
+
+  final VideoPost video;
+  const VideoButtons({
+    super.key,
+    required this.video,
+    });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.favorite)),
+      ],
+    );
   }
 }
