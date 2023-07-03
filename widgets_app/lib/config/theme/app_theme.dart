e75656437d8a2b2a11618c317402909153508786
Roque Molina <roque.nicolas.molina@gmail.com>
2023-07-03 14:23:42 -0300

    apptheme length

---
+++ widgets_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFFFF9000);
+
+const List<Color> colorList = [
+  _customColor,
+  Colors.blue,
+  Colors.yellow,
+  Colors.pink,
+  Colors.black,
+  Colors.green,
+  Colors.teal,
+  Colors.orange,
+  Colors.pinkAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+//constructor
+  AppTheme({
+    this.selectedColor = 0,
+    })
+    : assert(selectedColor >= 0,
+     'Selected color must be greater than 0'),
+      assert(selectedColor < colorList.length,
+     'Selected color must be smaller than available color list');
+
+  ThemeData getTheme() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: colorList[selectedColor],
+      brightness: Brightness.light,
+    );
+  }
+}
--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -11,11 +13,12 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 8).getTheme(),
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: FilledButton(onPressed: () {}, child: Text('mas')),
         ),
       ),
     );
