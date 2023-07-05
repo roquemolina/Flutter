67eaa546e6067995e79f26ff9f599bf82a1ee232
Roque Molina <roque.nicolas.molina@gmail.com>
2023-07-04 23:45:06 -0300

    CardsFinished

--- widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -86,7 +86,34 @@ class _ButtonView extends StatelessWidget {
                 style: ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(colors.primary),
                 )),
+            const CustomButton(),
           ])),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'HolaMundo',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
